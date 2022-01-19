e9b2a95f1bd27dc72f14d068aec91d294a74d090
kineretasiyahan <kineret9688@gmail.com>
2022-01-19 22:24:41 +0200

    add new changes

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ const express = require('express');
 const moviesRouter = require('./routes/movieRouter');
 const app = express();
 const path = require("path");
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 
-app.use('/',moviesRouter)
+app.use('/', moviesRouter)
 app.listen(PORT, (error) => {
     if (error) throw error;
     console.log(`Server is running on port ${PORT}`)
@@ -19,8 +19,7 @@ app.listen(PORT, (error) => {
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('*', (req, res)=>{
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
-  }
-  
+}
