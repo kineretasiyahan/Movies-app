cf619a8763a032947e16bf08fb6ec6463101ec3e
kineretasiyahan <kineret9688@gmail.com>
2022-01-19 21:55:08 +0200

    add new changes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const express = require('express');
 const moviesRouter = require('./routes/movieRouter');
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT | 5000;
 
 app.use(express.json());
