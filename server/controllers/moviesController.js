0a9ceff32edd74751ffaae569d6925798a66508b
kineretasiyahan <kineret9688@gmail.com>
2022-01-20 13:57:26 +0200

    new changes

--- server/controllers/moviesController.js
+++ server/controllers/moviesController.js
@@ -4,7 +4,7 @@ const API_KEY3 = process.env.API_KEY3;
 
 const getAllMovies = async (req, res) => {
     try {
-        const api_url = `https://imdb-api.com/en/API/MostPopularMovies/${API_KEY}`;
+        const api_url = `https://imdb-api.com/en/API/MostPopularMovies/k_8kg6mdib`;
         const fetch_response = await fetch(api_url).then((res) => {
             return res.json();
         })
@@ -27,7 +27,7 @@ const getAllMovies = async (req, res) => {
 const getMovieById = async (req, res) => {
     try {
         console.log(req.params.id);
-        const api_url = `https://www.omdbapi.com/?i=${req.params.id}&apikey=${API_KEY3}`;
+        const api_url = `https://www.omdbapi.com/?i=${req.params.id}&apikey=7edb05ae`;
         const fetch_response = await fetch(api_url).then((res) => {
             return res.json();
         })
