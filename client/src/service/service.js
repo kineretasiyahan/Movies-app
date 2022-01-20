aa026cf3e51e0fb930cd4edbac37d62b0c384b4a
kineretasiyahan <kineret9688@gmail.com>
2022-01-20 12:08:19 +0200

    fixed url in service

--- client/src/service/service.js
+++ client/src/service/service.js
@@ -1,10 +1,14 @@
+const API =
+  process.env.NODE_ENV === "production"
+    ? "https://movies-react-app-one.herokuapp.com/"
+    : "http://localhost:5000";
 export const getAllMovies = async () => {
-    return await fetch('http://localhost:5000/MostPopularMovies')
+    return await fetch(`${API}/MostPopularMovies`)
       .then((response) => response.json())
       .catch((error) => console.log(error));
   };
 export const getMovieById = async (movieId) => {
-    return await fetch(`http://localhost:5000/ByIdMovies/${movieId.id}`)
+    return await fetch(`${API}/ByIdMovies/${movieId.id}`)
       .then((response) => response.json())
       .then((response) => response.data)
       .catch((error) => console.log(error));
