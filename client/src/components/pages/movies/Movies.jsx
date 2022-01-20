a9445e0d91f7e1aa2e8a73b160ad50b5107e3b31
kineretasiyahan <kineret9688@gmail.com>
2022-01-20 13:05:31 +0200

    add heroku url

--- client/src/components/pages/movies/Movies.jsx
+++ client/src/components/pages/movies/Movies.jsx
@@ -23,7 +23,7 @@ const Movies = ({ movies }) => {
         />
       ) : 
         <div className="mainDiv">
-          {movies.map((item) => {
+          {movies?.map((item) => {
             return (
               <Card
                 className="card"
